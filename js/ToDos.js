183964cbdb31a3843b81b7883e6fe7721749c652
rus19023 <rus19023@byui.edu>
2022-03-26 15:52:55 -0400

    new background

--- js/ToDos.js
+++ js/ToDos.js
@@ -1,6 +1,21 @@
 import * as ls from "./ls.js";
 import * as util from "./utilities.js";
 
+// make some waves.
+var ocean = document.getElementById("ocean"),
+    waveWidth = 10,
+    waveCount = Math.floor(window.innerWidth/waveWidth),
+    docFrag = document.createDocumentFragment();
+
+for(var i = 0; i < waveCount; i++){
+  var wave = document.createElement("div");
+  wave.className += " wave";
+  docFrag.appendChild(wave);
+  wave.style.left = i * waveWidth + "px";
+  wave.style.webkitAnimationDelay = (i/100) + "s";
+}
+ocean.appendChild(docFrag);
+
 let todoList = [];
 //const lskey = 'items';
 var customtasks = [
@@ -344,3 +359,4 @@ function deleteTodo(id) {
     console.log(todoList);
     location.reload();
 }
+
